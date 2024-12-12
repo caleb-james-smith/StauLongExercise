bfb8385dd18ff9e52f9bbf434f902f7a434132b9
Zachary <zacharyflowers96@gmail.com>
2024-12-12 09:54:57 -0600

    update condor processing for lpc

--- Analysis/scripts/runNtuplizer.py
+++ Analysis/scripts/runNtuplizer.py
@@ -5,33 +5,42 @@ import optparse
 import shutil
 import random
 from glob import glob
-from sus-pag-long-exercise.Analysis.EraConfig import *
+from StauLongExercise.Analysis.EraConfig import *
 
+cmssw=os.environ['CMSSW_BASE']
+
+def makeSandbox(FarmDirectory,rand):
+    sandbox_cmd = f"tar --exclude='CMSSW_13_0_10/src/*' --exclude-caches-all -cf {FarmDirectory}/CMSSW_13_0_10_{rand}.tar -C $CMSSW_BASE/../ CMSSW_13_0_10"
+    os.system(sandbox_cmd)
+    sandbox_cmd = f"tar -rf {FarmDirectory}/CMSSW_13_0_10_{rand}.tar -C $CMSSW_BASE/../ CMSSW_13_0_10/src/PhysicsTools/ CMSSW_13_0_10/src/StauLongExercise/Analysis/python/DiTau_analysis.py CMSSW_13_0_10/src/StauLongExercise/Analysis/python/objectSelector.py CMSSW_13_0_10/src/StauLongExercise/Analysis/muon_Z.json CMSSW_13_0_10/src/StauLongExercise/Analysis/tau_DeepTau2018v2p5_2022_postEE.json"
+    os.system(sandbox_cmd)
+    sandbox_cmd = f"gzip {FarmDirectory}/CMSSW_13_0_10_{rand}.tar"
+    os.system(sandbox_cmd)
 
 def buildCondorFile(opt,FarmDirectory):
 
     """ builds the condor file to submit the ntuplizer """
 
-    cmssw=os.environ['CMSSW_BASE']
     rand='{:03d}'.format(random.randint(0,123456))
+    print(f"making sandbox {FarmDirectory}/CMSSW_13_0_10_{rand}.tar.gz")
+    makeSandbox(FarmDirectory,rand)
 	
     datasets=open(opt.input).read().splitlines()
-
     #condor submission file
     condorFile='%s/condor_generator_%s.sub'%(FarmDirectory,rand)
     print('Writes: %s'%condorFile)
     with open (condorFile,'w') as condor:
         condor.write('executable = {0}/worker_{1}.sh\n'.format(FarmDirectory,rand))
-        condor.write('output     = {0}/output{1}.out\n'.format(FarmDirectory,rand))
-        condor.write('error      = {0}/output{1}.err\n'.format(FarmDirectory,rand))
-        condor.write('log        = {0}/output{1}.log\n'.format(FarmDirectory,rand))
-        condor.write('+JobFlavour = "tomorrow"\n')
-        condor.write('+AccountingGroup = "group_u_CMST3.all"\n')
+        condor.write('output = {0}/output_{1}.out\n'.format(FarmDirectory,rand))
+        condor.write('error = {0}/output_{1}.err\n'.format(FarmDirectory,rand))
+        condor.write('log = {0}/output_{1}.log\n'.format(FarmDirectory,rand))
+        condor.write('use_x509userproxy = true\n')
         OpSysAndVer = str(os.system('cat /etc/redhat-release'))
         OpSysAndVer = "AlmaLinux9"
         condor.write('requirements = (OpSysAndVer =?= "{0}")\n\n'.format(OpSysAndVer))
         condor.write('should_transfer_files = YES\n')
-        condor.write('transfer_input_files = %s\n\n'%os.environ['X509_USER_PROXY'])
+        condor.write('when_to_transfer_output = ON_EXIT\n')
+        condor.write(f'transfer_input_files = {cmssw}/src/StauLongExercise/Analysis/scripts/keep_in.txt,{cmssw}/src/StauLongExercise/Analysis/scripts/keep_out.txt,{FarmDirectory}/CMSSW_13_0_10_{rand}.tar.gz\n')
         for dataset in datasets:
           if "#" in dataset or len(dataset)<2: continue
           print('INFO: Processing %s'%(dataset))
@@ -43,7 +52,7 @@ def buildCondorFile(opt,FarmDirectory):
             sufix='data'
             cmd='dasgoclient --query=\"file dataset={} status=*\"'.format(dataset)
             file_list=os.popen(cmd).read().split()
-            prefix='root://cms-xrd-global.cern.ch/'
+            prefix='root://cmsxrootd.fnal.gov/'
           elif 'eos' in dataset.split('/'):
             sufix='mc'
             dataset_name = dataset.split('/')[-1]
@@ -71,46 +80,52 @@ def buildCondorFile(opt,FarmDirectory):
               outfile='%s/%s'%(output_full,os.path.basename(file).replace('.root','_Skim.root'))
               if os.path.isfile(outfile) and not opt.force: continue
               condor.write('arguments = %s %s %s %s\n'%(prefix+file,'analysis_'+channel+sufix,output_full,filter))
+              skim_output_name = file.split('/')[-1:][0].split('.')[0]+"_Skim.root"
+              condor.write(f'transfer_output_files={skim_output_name}\n')
+              condor.write(f'transfer_output_remaps="{skim_output_name}={output_full}/{skim_output_name}"\n')
               condor.write('queue 1\n')
-
+              if opt.test:
+                break
+            if opt.test:
+              break
+          if opt.test:
+            break
 
     workerFile='%s/worker_%s.sh'%(FarmDirectory,rand)
     with open(workerFile,'w') as worker:
         worker.write('#!/bin/bash\n')
         worker.write('startMsg="Job started on "`date`\n')
         worker.write('echo $startMsg\n')
-        worker.write('export HOME=%s\n'%os.environ['HOME']) #otherwise, 'dasgoclient' won't work on condor
-        worker.write('export X509_USER_PROXY=%s\n'%os.environ['X509_USER_PROXY'])
+        worker.write('WORKDIR=$(pwd)\n')
         worker.write('########### INPUT SETTINGS ###########\n')
         worker.write('input=${1}\n')
         worker.write('channel=${2}\n')
-        worker.write('output=${3}\n')
+        worker.write('output=./\n')
         worker.write('filter=${@:4}\n')
         worker.write('filename=`echo ${1} | rev | cut -d"/" -f1 | rev | cut -d"." -f1`\n')
         worker.write('######################################\n')
         worker.write('echo "worker_%s.sh arguments:"\n'%(rand))
         worker.write('echo input="$input"\necho channel="$channel"\necho output="$output"\necho filter="$filter"\n')
         worker.write('######################################\n')
-        worker.write('WORKDIR=/tmp/%s/${filename}; mkdir -pv $WORKDIR\n'%os.environ['USER'])
-        worker.write('echo "Working directory is ${WORKDIR}"\n')
-        worker.write('cd %s\n'%cmssw)
-        worker.write('eval `scram r -sh`\n')
-        worker.write('cd ${WORKDIR}\n')
+        worker.write('source /cvmfs/cms.cern.ch/cmsset_default.sh\n')
+        worker.write(f'tar -xf CMSSW_13_0_10_{rand}.tar.gz\n')
+        worker.write(f'rm CMSSW_13_0_10_{rand}.tar.gz\n')
+        worker.write('cd CMSSW_13_0_10/src/\n')
+        worker.write('scramv1 b ProjectRename\n')
+        worker.write('eval `scramv1 runtime -sh`\n')
         worker.write('echo "INFO: Run ntuplizer"\n')
         worker.write('echo "python3 $CMSSW_BASE/src/PhysicsTools/NanoAODTools/scripts/nano_postproc.py \\\\"\n')
         worker.write('echo "$filename ${input}  \\\\"\n')
-        worker.write('echo "--bi $CMSSW_BASE/src/sus-pag-long-exercise/Analysis/scripts/keep_in.txt   \\\\"\n')
-        worker.write('echo "--bo $CMSSW_BASE/src/sus-pag-long-exercise/Analysis/scripts/keep_out.txt  \\\\"\n')
-        worker.write('echo "${filter} -I sus-pag-long-exercise.Analysis.DiTau_analysis ${channel} "\n')
+        worker.write('echo "--bi $WORKDIR/keep_in.txt   \\\\"\n')
+        worker.write('echo "--bo $WORKDIR/keep_out.txt  \\\\"\n')
+        worker.write('echo "${filter} -I StauLongExercise.Analysis.DiTau_analysis ${channel} "\n')
         worker.write('python3 $CMSSW_BASE/src/PhysicsTools/NanoAODTools/scripts/nano_postproc.py \\\n')
         worker.write('$filename ${input}  \\\n')
-        worker.write('--bi $CMSSW_BASE/src/sus-pag-long-exercise/Analysis/scripts/keep_in.txt   \\\n')
-        worker.write('--bo $CMSSW_BASE/src/sus-pag-long-exercise/Analysis/scripts/keep_out.txt  \\\n')
-        worker.write('${filter} -I sus-pag-long-exercise.Analysis.DiTau_analysis ${channel} \n')
-        worker.write('echo cp ${filename}/${filename}_Skim.root ${output}/${filename}_Skim.root\n')
-        worker.write('cp ${filename}/${filename}_Skim.root ${output}/\n')
-        worker.write('\necho clean output\ncd ../\nrm -rf ${WORKDIR}\n')
-        worker.write('echo ls; ls -l $PWD\n')
+        worker.write('--bi $WORKDIR/keep_in.txt   \\\n')
+        worker.write('--bo $WORKDIR/keep_out.txt  \\\n')
+        worker.write('${filter} -I StauLongExercise.Analysis.DiTau_analysis ${channel} \n')
+        worker.write('mv ${filename}/*.root $WORKDIR/\n')
+        worker.write('echo made output file: ${filename}_Skim.root\n')
         worker.write('echo $startMsg\n')
         worker.write('echo job finished on `date`\n')
     os.system('chmod u+x %s'%(workerFile))
@@ -124,6 +139,7 @@ def main():
       sys.exit(0)
     
     cmssw=os.environ['CMSSW_BASE']
+    user=os.environ['USER']
 
     #configuration
     usage = 'usage: %prog [options]'
@@ -131,10 +147,11 @@ def main():
     parser.add_option('-i', '--in',     dest='input',  help='list of input datasets',    default='listSamples.txt', type='string')
 
     # CHANGE: indicate the area in eos where the output files should be stored (and create it with mkdir before submitting the jobs)
-    parser.add_option('-o', '--out',      dest='output',   help='output directory',  default='/eos/cms/store/cmst3/user/ccaillol/cmsdas2024/ntuples_mutau_2022', type='string')
+    parser.add_option('-o', '--out',      dest='output',   help='output directory',  default=cmssw+'/src/cmsdas2025/ntuples_mutau_2022', type='string')
 
     parser.add_option('-f', '--force',      dest='force',   help='force resubmission',  action='store_true')
     parser.add_option('-s', '--submit',   dest='submit',   help='submit jobs',       action='store_true')
+    parser.add_option('-t', '--test',     dest='test',    help='only create single test job', action='store_true')
     (opt, args) = parser.parse_args()
      
     if not os.path.isfile(opt.input): 
@@ -144,12 +161,12 @@ def main():
     #prepare directory with scripts
     FarmDirectory=os.environ['PWD']+'/FarmLocalNtuple'
     if not os.path.exists(FarmDirectory):  os.system('mkdir -vp '+FarmDirectory)
-    print('\nINFO: IMPORTANT MESSAGE: RUN THE FOLLOWING SEQUENCE:')
-    print('voms-proxy-init --voms cms --valid 72:00 --out %s/myproxy509\n'%FarmDirectory)
-    os.environ['X509_USER_PROXY']='%s/myproxy509'%FarmDirectory
+    #os.environ['X509_USER_PROXY']='%s/myproxy509'%FarmDirectory
 
     #build condor submission script and launch jobs
     condor_script=buildCondorFile(opt,FarmDirectory)
+    #print('\nINFO: IMPORTANT MESSAGE: RUN THE FOLLOWING SEQUENCE:')
+    #print('voms-proxy-init --voms cms --valid 72:00 --out %s/myproxy509\n'%FarmDirectory)
 
     #submit to condor
     if opt.submit:
