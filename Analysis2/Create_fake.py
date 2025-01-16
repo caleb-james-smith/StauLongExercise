4e64da36e5d038b0b712993d434013bc280ed6f6
Caleb James Smith <colorado.caleb.smith@gmail.com>
2025-01-16 14:19:02 -0600

    New function to make fakes.

--- Analysis2/Create_fake.py
+++ Analysis2/Create_fake.py
@@ -1,19 +1,34 @@
 import ROOT
 
-
 fVV=ROOT.TFile("output_mutau_2022/WW_postEE.root","r")
 fTT=ROOT.TFile("output_mutau_2022/TTTo2L2Nu_postEE.root","r")
 fDY=ROOT.TFile("output_mutau_2022/DY_postEE.root","r")
 fData=ROOT.TFile("output_mutau_2022/Data.root","r")
 fout=ROOT.TFile("output_mutau_2022/Fake.root","recreate")
 
-dir0=fout.mkdir("OSiso")
+def make_fakes(dir_name, anti_dir_name):
 
-h0=fData.Get("OSanti/data_obs")
-#CHANGE subtract MC prediction from other prompt backgrounds (Drell-Yan, diboson, ttbar, ...)
+    dir0=fout.mkdir(dir_name)
 
-fout.cd()
-dir0.cd()
-h0.SetName("Fake")
-h0.Write()
+    h0=fData.Get(anti_dir_name + "/data_obs")
+    #CHANGE subtract MC prediction from other prompt backgrounds (Drell-Yan, diboson, ttbar, ...)
+    h1=fVV.Get(anti_dir_name + "/WW")
+    h2=fTT.Get(anti_dir_name + "/TTTo2L2Nu")
+    h3=fDY.Get(anti_dir_name + "/DY")
+
+    hsum = h1.Clone()
+    hsum.Add(h2)
+    hsum.Add(h3)
+    
+    hfake = h0.Clone()
+    hfake.Add(hsum, -1)
+
+    fout.cd()
+    dir0.cd()
+    hfake.SetName("Fake")
+    hfake.Write()
+    return
+
+make_fakes("OSiso", "OSanti")
+#make_fakes("dz_mu", "anti_dz_mu")
 
